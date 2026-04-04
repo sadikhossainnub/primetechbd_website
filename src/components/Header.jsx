eee3b2f24f1885b50390f26df95759173509a6fe
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-04-05 03:06:57 +0600

    feat: add Header component and update Dockerfile base image syntax

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,6 @@ const Header = () => {
                     <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Us</Link></li>
                     <li><Link to="/vision-mission" className={location.pathname === '/vision-mission' ? 'active' : ''}>Vision & Mission</Link></li>
                     <li><Link to="/team" className={location.pathname === '/team' ? 'active' : ''}>Our Team</Link></li>
-                    <li><Link to="/request" className={location.pathname === '/request' ? 'active' : ''}>Request Software</Link></li>
                 </ul>
                 <Link to="/request" className="btn-contact">Get Started</Link>
             </nav>
